b5139af441481874bad9e0ccafb01394430a0666
Hari Sood <hsood@turing.ac.uk>
2024-02-20 14:35:47 +0000

    Ignore links pre-commit

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ linkcheck_ignore = [
     # GitHub CI linkchecker seems to be blocked
     r"https://www.turing.ac.uk/.*",
     r"https://www.hpe.com/.*",
-    r"https://csrc.nist.gov/.*"
+    r"https://csrc.nist.gov/.*",
 ]
 
 # These pages use in-page JavaScript anchors which aren't seen by the link checker
