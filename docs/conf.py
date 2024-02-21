d886848dd62d9c0b4feb8ca234d5fd3deeb8f92d
Simon Li <orpheus+devel@gmail.com>
2024-02-21 16:35:16 +0000

    Fix build warnings

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 myst_enable_extensions = ["fieldlist", "linkify"]
 myst_linkify_fuzzy_links = False
 
+# Automatically create anchors for in-page headings up to level 3
+myst_heading_anchors = 4
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
